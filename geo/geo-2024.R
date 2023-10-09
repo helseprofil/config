939c4ffc557df13d674680faa3a8e6fafc4f7575
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2023-10-09 09:34:58 +0200

    Clean up geo split 2024

--- geo/geo-2024.R
+++ geo/geo-2024.R
@@ -1,4 +1,9 @@
-## Geo splitt for 2024
+## There are geo splitt for 2024
 ## See norgeo issue 84
+## Input dataset is DT and output row index must be IDX
 
-idx <- c(37, 69, 70, 71, 73, 74)
+## Find row index to be deleted from dataset
+x1 <- DT[oldCode == 1534 & currentCode == 1508, which = TRUE]
+x2 <- DT[oldCode %in% c(1504, 1523, 1529, 1546, 1507) & currentCode == 1580, which = TRUE]
+
+IDX <- c(x1, x2)

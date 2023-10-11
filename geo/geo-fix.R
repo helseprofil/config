f0ac6c85037048bdbc18f755f89c6273be2b842c
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2023-10-11 13:33:16 +0200

    Fix 1507 changes in 2020

--- geo/geo-fix.R
+++ geo/geo-fix.R
@@ -1,2 +1,7 @@
 ## Fix geo codes manually
 ## Input and output file should be nammed as DT
+
+## Geo 2024 for Haram and Ålesund
+DT[oldCode == 1534 & currentCode == 1507, currentCode := 1580] #Haram
+DT[oldCode %in% c(1504, 1523, 1529, 1546) & currentCode == 1580, currentCode := 1508]
+DT[oldCode == 1507, currentCode := 1599]
